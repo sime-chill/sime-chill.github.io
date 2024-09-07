a3d8aeda32ff0c1d052cbeaac9b17a6cfb98770b
sime-chill <heyh39@mail2.sysu.edu.cn>
2024-09-07 21:07:01 +0800

    updated

--- etached/assets/js/site.js
+++ etached/assets/js/site.js
@@ -43,25 +43,25 @@ function reverseString(str) {
 /**
  * Moves WeChat widget
  */
-// function moveWidget(event) {
-//   var w = $("#wechat-widget");
+function moveWidget(event) {
+  var w = $("#wechat-widget");
 
-//   w.css({
-//     left: event.pageX - 25,
-//     top: event.pageY - w.height() - 60
-//   });
-// }
+  w.css({
+    left: event.pageX - 25,
+    top: event.pageY - w.height() - 60
+  });
+}
 
-// $("a#wechat-link").on("mouseenter", function(event) {
-//   $("#wechat-widget").css({ display: "block" });
+$("a#wechat-link").on("mouseenter", function(event) {
+  $("#wechat-widget").css({ display: "block" });
 
-//   moveWidget(event);
-// });
+  moveWidget(event);
+});
 
-// $("a#wechat-link").on("mousemove", function(event) {
-//   moveWidget(event);
-// });
+$("a#wechat-link").on("mousemove", function(event) {
+  moveWidget(event);
+});
 
-// $("a#wechat-link").on("mouseleave", function(event) {
-//   $("#wechat-widget").css({ display: "none" });
-// });
+$("a#wechat-link").on("mouseleave", function(event) {
+  $("#wechat-widget").css({ display: "none" });
+});
--- index.md
+++ index.md
@@ -1,11 +1,14 @@
 ---
 layout: home
 profile_picture:
-  src: /assets/img/profile-pic.jpg
-  alt: website picture
+  src: /assets/img/pic_hyh1.png
+  alt: pic_hyh1
 ---
 
 <p>
-  Welcome!
+I am a second-year M.S. student studying electronic and information engineering in the <a href="http://www.it.fudan.edu.cn/">School of Information Science and Technology</a> at Fudan University (FDU) where I am advised by <a href="http://www.it.fudan.edu.cn/Data/View/1139">Prof. Zhuo Zou</a>. Before that, I received my B.E. degree in microelectronics science and engineering from the <a href="https://seit.sysu.edu.cn/">School of Electronics and Information Technology (School of Microelectronis)</a> at Sun Yat-sen University (SYSU).
 </p>
 
+<p>
+I have a broad interest in low-power digital intergrated circuit (IC) design and the memory-centric neuromorphic computing architecture. 
+</p>
